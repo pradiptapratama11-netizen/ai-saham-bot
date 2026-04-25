5644ef9b7374b31314f8d68e2a701f9aa4c0d644
pradiptapratama11-netizen <pradiptapratama11@gmail.com>
2026-04-26 01:06:20 +0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -13,6 +13,20 @@ from sklearn.ensemble import RandomForestClassifier
 
 BOT_TOKEN=os.getenv("BOT_TOKEN")
 CHAT_ID=str(os.getenv("CHAT_ID"))
+import requests
+
+r=requests.post(
+f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+json={
+"chat_id":CHAT_ID,
+"text":"TEST GITHUB BOT"
+}
+)
+
+print(r.status_code)
+print(r.text)
+
+raise SystemExit
 
 
 def kirim(msg):
